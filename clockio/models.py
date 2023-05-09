671019634583e5faa98c42e3b6015fc0d79c43b3
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-09 14:42:56 -0300

    feat: Modelo inicial de User

--- clockio/models.py
+++ clockio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True)
+    email = models.EmailField(null=False, blank=False, unique=True)
+    password = models.CharField(null=False, blank=False)
+    first_name = models.CharField(null=False, blank=False)
+    last_name = models.CharField(null=False, blank=False)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f'\nid: {self.id}\nemail: {self.email}\nfirst_name: {self.first_name}\nlast_name: {self.last_name}\nis_active: {self.is_active}'
