2f48a8f415d91bc135094c3ee0707df939fb4298
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-09 15:58:33 -0300

    fix: Adicionando ClockioConfig ao arquivo settings

--- setup/settings.py
+++ setup/settings.py
@@ -18,7 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clockio',
+    'clockio.apps.ClockioConfig',
 ]
 
 MIDDLEWARE = [
