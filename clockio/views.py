241ef7141a95065dc95b5b8e40fd6a9e2b8fc1e7
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-05-09 16:09:51 -0300

    feat: Consumindo registros do banco de dados pelas views

--- clockio/views.py
+++ clockio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from clockio.models import User
 
 
 def index(request):
-    return render(request, 'clockio/index.html')
+    users = User.objects.all()
+    return render(request, 'clockio/index.html', {'users': users})
