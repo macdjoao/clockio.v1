fa4ee6b75ce27c3e31f02abe4d35cc4e2e4269f0
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-17 18:51:19 -0300

    fix: Organizando urls

--- clockio/urls.py
+++ clockio/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from employee.views import home, about, contact
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
-    path('about/', about),
-    path('contact/', contact)
+    path('', include('employee.urls'))
 ]
