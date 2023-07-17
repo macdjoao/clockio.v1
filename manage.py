8d4e6035e52c102c191710b32472f8ec32dcb07f
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-17 17:15:23 -0300

    fix: Reiniciando projeto Django

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setup.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clockio.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
