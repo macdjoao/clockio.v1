65492b55401cde252077e6454eb8248cfb3e2cfa
João Victor Inocêncio Macedo de Aguiar <macdjoao@gmail.com>
2023-07-25 20:57:13 -0300

    feat: Esboço dos modelos

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Role(models.Model):
+    title = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Clock(models.Model):
+    slug = models.SlugField()
+    is_published = models.BooleanField(default=True)
+    entry_date = models.DateTimeField()
+    departure_date = models.DateTimeField()
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.user
+
+# Para trabalhar com imagens
+# poetry add Pillow
+# img = models.ImageField(upload_to='employee/imgs/%Y/%m/%d/')
